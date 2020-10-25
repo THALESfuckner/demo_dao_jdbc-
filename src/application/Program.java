e7ec8236eb6f11c2b62e48689138cfa0e08b8656
THALESfuckner <63377349+THALESfuckner@users.noreply.github.com>
2020-10-25 16:30:37 -0300

    alteração 6

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,7 @@
 package application;
 
-import java.sql.Date;
-
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
@@ -10,9 +9,11 @@ public class Program {
 
 
 	public static void main(String[] args) {
-		Department obj = new Department (1, "Books");
 		
-		Seller seller = new Seller (21, "bob", "henrique@gmail.com", new Date(0), 3333.0, obj );
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 		
